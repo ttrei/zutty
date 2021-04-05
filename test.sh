3a57a0cab8ffec980b0af7382fc42bce62e78ae5
Reinis Taukulis <reinis.taukulis@gmail.com>
2021-04-05 17:29:05 +0300

    test only 14pt

--- test.sh
+++ test.sh
@@ -11,16 +11,16 @@ test_font() {
     ./build/src/zutty.dbg -font DejaVuSansMono -fontsize $size -e exit && mv test.bmp testimages/dejavu-$size.bmp
 }
 
-test_font 12
-test_font 13
+#test_font 12
+#test_font 13
 test_font 14
-test_font 15
-test_font 16
-test_font 17
-test_font 18
-test_font 19
-test_font 20
-test_font 21
-test_font 22
-test_font 23
-test_font 24
+#test_font 15
+#test_font 16
+#test_font 17
+#test_font 18
+#test_font 19
+#test_font 20
+#test_font 21
+#test_font 22
+#test_font 23
+#test_font 24
