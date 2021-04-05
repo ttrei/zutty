b21d5698b1324331314fd5bcf543e3a0eb7de226
Reinis Taukulis <reinis.taukulis@gmail.com>
2021-04-05 15:40:41 +0300

    test more sizes

--- test.sh
+++ test.sh
@@ -2,7 +2,25 @@
 
 set -e
 
+mkdir -p testimages
+
 ./waf
-./build/src/zutty.dbg -font DejaVuSansMono -fontsize 16 -e exit && mv test.bmp dejavu-16.bmp
-./build/src/zutty.dbg -font DejaVuSansMono -fontsize 20 -e exit && mv test.bmp dejavu-20.bmp
-./build/src/zutty.dbg -font DejaVuSansMono -fontsize 21 -e exit && mv test.bmp dejavu-21.bmp
+
+test_font() {
+    size=$1
+    ./build/src/zutty.dbg -font DejaVuSansMono -fontsize $size -e exit && mv test.bmp testimages/dejavu-$size.bmp
+}
+
+test_font 12
+test_font 13
+test_font 14
+test_font 15
+test_font 16
+test_font 17
+test_font 18
+test_font 19
+test_font 20
+test_font 21
+test_font 22
+test_font 23
+test_font 24
